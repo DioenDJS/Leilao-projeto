54127d6876cde52bc9c61c0512aee1728d980a8e
DioenDJS <negociospacheti@gmail.com>
2021-10-31 11:14:21 -0300

    :art: configuring bootswatch import in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import "bootswatch/dist/united/bootstrap.min.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
