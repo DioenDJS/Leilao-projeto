f9a383c2b7dfc58c046d8499d69f11f0779989e2
DioenDJS <negociospacheti@gmail.com>
2021-11-06 13:15:27 -0300

    :heavy_plus_sign: install react-router-dom and router in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Titulo from './components/Titulo';
 import ListaObras from './components/ListaObras';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-    <Titulo />
-    <ListaObras />
-    </>
+    <Router>
+        <Titulo />
+      <Switch>
+        <Route path="/" exact>
+           <ListaObras />
+        </Route>
+        <Route path="/detalhes/:id">
+          <Detalhes/>
+        </Route> 
+      </Switch>
+    </Router>
   );
-}
+
+  
+};
 
 export default App;
